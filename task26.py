957052f52077b1a6815b86d4e17b01a34167b2cf
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-04-25 12:59:31 +0300

    task_add is done, damn it!

--- task26.py
+++ task26.py
@@ -5,73 +5,33 @@
 def fact_and_triangle_fact(n):
     res_f = n
     count = n
-    #print('res_f = ', res_f, ", " 'res_t = ', res_t)
 
     if n > 1 :
-        #print('вошли в if +++++++++++++++++++++++++++++++')
-        #print('n = ', n, ' ', 'count =', count, 'res_f = ', res_f)#, 'res_t = ', res_t)
-        
-        #print(f'res_f = {res_f}, n = {n} ')
         res_f = res_f * fact_and_triangle_fact(count-1)
-        #res_t = res_t + fact_and_triangle_fact(count-1)
-        #print(fact_and_triangle(n-1))
-        #print(f'res_f = {res_f}')
-        #input('жми...')
-        #print(f'res_f = res_f * (n - 1) = {res_f}')
-        #res_t = n + (n-1)
-        #count += 1
-        
-        
-        #print('res_f = ', res_f, 'res_t = ', res_t)
+        print('count = ', count)
         return res_f
-        #return res_t
     else:
-        
         return res_f
-        # return res_t
-        #print(res_f)
-        #print(res_t)
 
 def fact_and_triangle_triangle(n):
     res_t = n
     count = n
-    #n -= 1
-    #print('res_t = ', res_t)
 
     if n > 0:
-        #print('вошли в if +++++++++++++++++++++++++++++++')
-        #print('n = ', n, ' ', 'count =', count,  'res_t = ', res_t)
-        
-        #print(f'res_f = {res_f}, n = {n} ')
-        
         res_t = res_t + fact_and_triangle_triangle(count-1)
-        #print(fact_and_triangle_triangle(n-1))
-        #print(f'res_t = {res_t}')
-        #input('жми...')
-        #print(f'res_f = res_f * (n - 1) = {res_f}')
-        #res_t = n + (n-1)
-        #count += 1
-        
-        
-        #print('res_t = ', res_t)
         return  res_t
     else:
-        
         return  res_t
-        # return res_t
-        
-        #print(res_t)
 
 def fact_and_triangle(n):
     res_f = fact_and_triangle_fact(n)
-    print(res_f)
     res_t = fact_and_triangle_triangle(n)
     res = [res_f, res_t]
     return res
 
     
 print()
-n = 5
+n = int(input('Введите число: '))
 print(f'Факториал числа {n}: {fact_and_triangle(n)[0]}')
 print(f'Треугольное число числа {n}: {fact_and_triangle(n)[1]}')
 print()
